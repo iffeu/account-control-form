5acc6ff18bdfbde088396685f93f62084206de6f
iffeu <143319004+iffeu@users.noreply.github.com>
2025-07-09 17:42:14 -0300

    Add primevue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
 
-<template></template>
+<template>
+  <Button>Verify</Button>
+</template>
 
 <style scoped></style>
