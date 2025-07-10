d73ddacea7d4490bb134978f388071391637648b
iffeu <143319004+iffeu@users.noreply.github.com>
2025-07-10 10:47:28 -0300

    Fix labels

--- src/components/AccountForm.vue
+++ src/components/AccountForm.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
 import { ACCOUNT_TYPES } from '@/types/Account'
-import { ref } from 'vue'
+import { useId } from 'vue'
 
-const login = ref('')
-const labels = ref('')
-const password = ref('')
-const type = ref('local')
+const login = useId()
+const labels = useId()
+const password = useId()
+const type = useId()
 </script>
 
 <template>
   <form>
     <fieldset>
       <legend>Аккаунт</legend>
-      <label for="labels">Метки</label>
-      Тип записи
-      <InputText id="labels" v-model="labels" aria-describedby="username-help" />
-      <Select v-model="type" :options="[...ACCOUNT_TYPES]" checkmark />
-      <label for="login">Логин</label>
-      <InputText id="login" v-model="login" aria-describedby="username-help" />
-      <label for="password">Пароль</label>
-      <Password id="password" v-model="password" toggleMask />
+
+      <label :for="labels">Метки</label>
+      <InputText :id="labels" aria-describedby="username-help" />
+
+      <label :for="type">Тип записи</label>
+      <Select :input-id="type" :options="[...ACCOUNT_TYPES]" checkmark />
+
+      <label :for="login">Логин</label>
+      <InputText :id="login" aria-describedby="username-help" />
+
+      <label :for="password">Пароль</label>
+      <Password :input-id="password" toggleMask />
     </fieldset>
     <Button icon="pi pi-trash" variant="text" rounded aria-label="Удалить" />
   </form>
