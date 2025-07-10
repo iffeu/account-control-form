4904bc125f8476952ab3cf74951d4f0ccf157a9f
iffeu <143319004+iffeu@users.noreply.github.com>
2025-07-10 20:04:36 -0300

    Add stringified account guards and convertors

--- src/types/Account.ts
+++ src/types/Account.ts
@@ -1,4 +1,4 @@
-import { accountSchema } from '@/schemas/Account.schema'
+import { accountSchema, stringifiedAccountSchema } from '@/schemas/Account.schema'
 
 interface AccountBase {
   labels: { text: string }[]
@@ -21,4 +21,47 @@ export type Account = AccountLDAP | AccountLocal
 
 export function isAccount(data: unknown): data is Account {
   return accountSchema.safeParse(data).success
+}
+
+export type StringifiedAccount = Omit<Account, 'labels'> & { labels: string }
+
+export function isStringifiedAccount(data: unknown): data is StringifiedAccount {
+  return stringifiedAccountSchema.safeParse(data).success
+}
+
+export function toStringifiedAccount(account: Account): StringifiedAccount {
+  return {
+    ...account,
+    labels: account.labels.map((label) => label.text).join('; '),
+  }
+}
+
+export function fromStringifiedAccount(account: StringifiedAccount): Account {
+  // here we change only the labels field, and that keeps
+  // account type and password pair, so we can use "as Account"
+  return {
+    ...account,
+    labels:
+      account.labels
+        .split(';')
+        .map((s) => s.trim())
+        .filter((s) => s.length)
+        .map((s) => ({ text: s })) ?? [],
+  } as Account
+}
+
+export function fromPartialStringifiedAccount(
+  account: Partial<StringifiedAccount>,
+): Partial<Account> {
+  // here we change only the labels field, and that keeps
+  // account type and password pair, so we can use "as Account"
+  return {
+    ...account,
+    labels:
+      account.labels
+        ?.split(';')
+        .map((s) => s.trim())
+        .filter((s) => s.length)
+        .map((s) => ({ text: s })) ?? [],
+  } as Partial<Account>
 }
