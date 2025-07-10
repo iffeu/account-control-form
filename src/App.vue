676a8d14661274df805c96b87d18dc85a77fe14c
iffeu <143319004+iffeu@users.noreply.github.com>
2025-07-10 20:33:42 -0300

    Improve page styles

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,57 @@ const { accounts } = storeToRefs(useAccountsStore())
 </script>
 
 <template>
-  <h2>Учетные записи</h2>
-  <Button @click="addAccount" icon="pi pi-plus" variant="outlined" aria-label="Добавить аккаунт" />
-  <Message severity="info"
-    >Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;</Message
-  >
+  <section class="accounts-info">
+    <div class="accounts-info__title-wrapper">
+      <h2 class="accounts-info__title">Учетные записи</h2>
+      <Button
+        @click="addAccount"
+        icon="pi pi-plus"
+        variant="outlined"
+        aria-label="Добавить аккаунт"
+      />
+    </div>
 
-  <ul>
-    <li v-for="(account, index) of accounts" :key="account.login">
-      <AccountForm :account @update="updateAccount(index, $event)" @delete="deleteAccount(index)" />
-    </li>
-  </ul>
+    <Message severity="info" icon="pi pi-question-circle"
+      >Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;</Message
+    >
+
+    <ul class="accounts-info__account-list">
+      <li v-for="(account, index) of accounts" :key="account.login">
+        <AccountForm
+          :account
+          @update="updateAccount(index, $event)"
+          @delete="deleteAccount(index)"
+        />
+      </li>
+    </ul>
+  </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.accounts-info {
+  max-width: 80%;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  gap: 16px;
+}
+
+.accounts-info__title-wrapper {
+  display: flex;
+  gap: 24px;
+}
+
+.accounts-info__title {
+  margin: 0;
+  padding: 0;
+}
+
+.accounts-info__account-list {
+  list-style: none;
+  padding-left: 0;
+  display: flex;
+  gap: 12px;
+  flex-direction: column;
+}
+</style>
