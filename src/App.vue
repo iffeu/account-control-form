a3192fe9ea37282c3459a2aa09dc72b6f4783d61
iffeu <143319004+iffeu@users.noreply.github.com>
2025-07-10 18:31:58 -0300

    Fix accounts render

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
 import AccountForm from './components/AccountForm.vue'
-import type { Account } from './types/Account'
+import { useAccountsStore } from './stores/accounts'
 
-const account: Account = {
-  labels: [{ text: 'XXX' }, { text: 'YYY' }],
-  type: 'local',
-  login: 'test',
-  password: 'test',
-}
+const { addAccount, updateAccount, deleteAccount } = useAccountsStore()
+const { accounts } = storeToRefs(useAccountsStore())
 </script>
 
 <template>
   <h2>Учетные записи</h2>
-  <Button icon="pi pi-plus" variant="outlined" aria-label="Добавить аккаунт" />
+  <Button @click="addAccount" icon="pi pi-plus" variant="outlined" aria-label="Добавить аккаунт" />
   <Message severity="info"
     >Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;</Message
   >
 
   <ul>
-    <li>
-      <AccountForm :account />
-    </li>
-    <li>
-      <AccountForm :account />
+    <li v-for="(account, index) of accounts" :key="account.login">
+      <AccountForm :account @update="updateAccount(index, $event)" @delete="deleteAccount(index)" />
     </li>
   </ul>
 </template>
