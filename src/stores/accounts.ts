68af757ddbf5fcef66acea8b6be4916afae7cc96
iffeu <143319004+iffeu@users.noreply.github.com>
2025-07-10 18:30:36 -0300

    Improve account store

--- src/stores/accounts.ts
+++ src/stores/accounts.ts
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
 import { isAccount, type Account } from '@/types/Account'
 import { isJSONString } from '@/utils/isJSONString'
@@ -6,7 +6,7 @@ import { isJSONString } from '@/utils/isJSONString'
 export const useAccountsStore = () => {
   const innerStore = defineStore('accounts', () => {
     const accounts = ref<Account[]>()
-
+    const readonlyAccounts = computed(() => accounts.value)
     const defaultAccount = ref<Account>({
       labels: [],
       login: '',
@@ -24,30 +24,44 @@ export const useAccountsStore = () => {
         }
       }
 
+      accounts.value = []
       localStorage.removeItem('accounts')
     }
 
     function addAccount() {
-      accounts.value?.push({ ...defaultAccount.value })
+      accounts.value?.push({
+        ...defaultAccount.value,
+        login: defaultAccount.value.login + `${Math.floor(Math.random() * 99999)}`,
+      })
     }
 
-    function deleteAccount(login: string) {
-      accounts.value = accounts.value?.filter((account) => account.login !== login)
+    function deleteAccount(index: number) {
+      console.log(index)
+      accounts.value = accounts.value?.filter((_, i) => i !== index)
+      console.log(accounts.value)
     }
 
     function updateAccount(index: number, updatedAccount: Partial<Account>) {
       accounts.value = accounts.value?.map((account, i) => {
         if (index === i) {
           const newAccount = { ...account, ...updatedAccount }
-          if (isAccount(newAccount)) {
-            return newAccount
+          const newAccountFormFormat = { ...newAccount, labels: newAccount.labels.join('; ') }
+          if (isAccount(newAccountFormFormat)) {
+            return newAccount as Account
           }
         }
         return account
       })
     }
 
-    return { accounts, defaultAccount, loadAccounts, addAccount, deleteAccount, updateAccount }
+    return {
+      accounts: readonlyAccounts,
+      defaultAccount,
+      loadAccounts,
+      addAccount,
+      deleteAccount,
+      updateAccount,
+    }
   })
 
   const store = innerStore()
